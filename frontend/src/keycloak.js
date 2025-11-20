9d5694633c901dd10893bb3f2d4575873174ead2
danielodg <danield317@unitec.edu>
2025-11-20 06:59:35 -0600

    agregando diseño a Login[

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
   url: "http://localhost:8080",
-  realm: "master",
-  clientId: "12345",
+  realm: "Hive",
+  clientId: "mi-app-de-prueba",
 });
 
 export default keycloak;
